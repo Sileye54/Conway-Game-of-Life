98e43ea8a8e4fe6af36e84a8fa7adccb2dcda6ab
guisse.sileye_lamine <jk091936@ens.uqam.ca>
2025-09-27 03:03:40 -0400

    Ajoute boardSize et getBoardSize dans ConwayArguments

--- src/main/java/conway/ConwayGameData.java
+++ src/main/java/conway/ConwayGameData.java
@@ -3,16 +3,21 @@ package conway;
 public class ConwayGameData extends GameData{
     private int numberOfIterations;
     private char[][] boardStructure;
+    private int boardSize;
 
     public ConwayGameData(int numberOfIterations, char boardStructure[][]){
         this.numberOfIterations = numberOfIterations;
         this.boardStructure = boardStructure;
+        this.boardSize = boardStructure.length;
     }
     public int getNumberOfIterations() {
         return numberOfIterations;
     }
     public char[][] getBoardStructure() {
         return boardStructure;
+    }
+    public int getBoardSize(){
+        return boardSize;
     }
 
 }
