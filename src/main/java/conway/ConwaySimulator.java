7e014cad0348136215f0a0a859cdd7e6291c644a
guisse.sileye_lamine <jk091936@ens.uqam.ca>
2025-09-28 18:43:26 -0400

    Ajoute les classes Simulator et ConwaySimulator

---
+++ src/main/java/conway/ConwaySimulator.java
@@ -0,0 +1,23 @@
+package conway;
+
+/**
+ * The ConwaySimulator class implements the Simulator interface to simulate the
+ * Game of Life.
+ */
+public class ConwaySimulator implements Simulator<ConwayBoard> {
+    @Override
+    public void simulate(ConwayBoard conwayBoard, int numberOfIterations) {
+        for (int i = 0; i < numberOfIterations; i++) {
+            ConwayCell[][] board = conwayBoard.getBoard();
+            ConwayCell[][] newBoard = conwayBoard.copy().getBoard();
+            for (int x = 0; x < board.length; x++) {
+                for (int y = 0; y < board[x].length; y++) {
+                    if (board[x][y].isStateSwitching(conwayBoard.getNeighborCells(board[x][y]))) {
+                        newBoard[x][y].switchState();
+                    }
+                }
+            }
+            conwayBoard.setBoard(newBoard);
+        }
+    }
+}
